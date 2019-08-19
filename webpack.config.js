0c8cb84a0d78d9cee6f3a777159949106a89c2f0
liuhaoa <liuhaoa@zbj.com>
2019-08-19 11:37:21 +0800

    updated version 7.0.1
    fix [Webpack](https://github.com/aui/artDialog/issues/344) 'configuration.output.path: The provided value "dist" is not an absolute path!'

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 var webpack = require('webpack');
 var version = require('./package.json').version;
+const path = require('path')
 
 module.exports = {
     entry: {
         'dialog': './src/dialog.js',
-        'dialog-plus': './src/dialog-plus.js'
+        'dialog-plus':'./src/dialog-plus.js'
     },
     output: {
-        path: 'dist',
+        path:path.resolve(__dirname, './dist'),
         filename: '[name].js',
         library: `dialog`,
         libraryTarget: 'umd'
